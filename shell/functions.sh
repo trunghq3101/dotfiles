598c601870f13622d2f21ae75d632494423c10ab
trunghq3101 <trunghq3101@gmail.com>
2022-02-12 09:22:42 +0700

    Use function to manually import SSH

--- shell/functions.sh
+++ shell/functions.sh
@@ -2,16 +2,27 @@
 
 # Encrypt secrets
 endot() {
+    cd ~/dotfiles
     tar czf ssh.tar.gz ssh 
     gpg -er trunghq3101@gmail.com ssh.tar.gz
     rm ssh.tar.gz
+    cd -
     echo "Done!"
 }
 
 # Decrypt secrets
 dedot() {
+    cd ~/dotfiles
     gpg -do ssh.tar.gz ssh.tar.gz.gpg
     tar xvf ssh.tar.gz
     rm ssh.tar.gz
+    cd -
     echo "Done!"
+}
+
+# Import SSH
+import_ssh() {
+    dedot
+    cp -afi ssh/. ~/.ssh/
+    chmod -R 0700 ssh ~/.ssh
 }
